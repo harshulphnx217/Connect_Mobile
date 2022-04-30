21accd3f13bf3be429912d1200609d6ad2de064f
Blake2912 <kcvarun29122001@gmail.com>
2022-04-30 20:12:59 +0530

    Add: Dependencies for Volley and Glide Created Home Screen Activity

---
+++ app/src/main/java/com/connect/connect/HomeScreen.kt
@@ -0,0 +1,11 @@
+package com.connect.connect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+    }
+}
