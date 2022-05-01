0daca112021196d7bf1105130e94f17f9f2014f7
Blake2912 <kcvarun29122001@gmail.com>
2022-05-01 18:29:21 +0530

    Home Page UI Complete

--- app/src/main/java/com/connect/connect/HomeScreen.kt
+++ app/src/main/java/com/connect/connect/HomeScreen.kt
@@ -2,10 +2,18 @@ package com.connect.connect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class HomeScreen : AppCompatActivity() {
+
+    private lateinit var postRecyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
+
+        postRecyclerView = findViewById(R.id.post_recycler_view);
+        postRecyclerView.adapter = PostAdapter()
+
     }
 }
