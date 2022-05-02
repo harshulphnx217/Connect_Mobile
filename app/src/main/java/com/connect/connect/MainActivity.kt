0df88e69f6af0e8f8922a10146c585ccafd99f1f
Blake2912 <kcvarun29122001@gmail.com>
2022-05-02 19:03:34 +0530

    Corrected Register and Login UI

--- app/src/main/java/com/connect/connect/MainActivity.kt
+++ app/src/main/java/com/connect/connect/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.connect.connect
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var goToRegister: Button
+    private lateinit var loginBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        goToRegister = findViewById(R.id.register_btn)
+        goToRegister.setOnClickListener{
+            startActivity(Intent(this,Register::class.java))
+        }
+
+        loginBtn = findViewById(R.id.login_btn)
+        loginBtn.setOnClickListener{
+            startActivity(Intent(this,HomeScreen::class.java))
+        }
     }
 }
