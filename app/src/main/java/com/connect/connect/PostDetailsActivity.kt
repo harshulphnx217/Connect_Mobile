01f73a25ce0378f8538f1762843ad78794bd9543
Blake2912 <kcvarun29122001@gmail.com>
2022-05-02 19:05:46 +0530

    Conflict Resolved

---
+++ app/src/main/java/com/connect/connect/PostDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.connect.connect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PostDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_details)
+    }
+}
