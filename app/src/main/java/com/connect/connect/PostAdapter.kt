b562e7481624f95ab5a68a5d58f9486d31a19d28
Blake2912 <kcvarun29122001@gmail.com>
2022-05-02 19:50:30 +0530

    Add: Post Details Screen and Comment Adapter Configured the application for Dark Mode

--- app/src/main/java/com/connect/connect/PostAdapter.kt
+++ app/src/main/java/com/connect/connect/PostAdapter.kt
@@ -1,19 +1,21 @@
 package com.connect.connect
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.RecyclerView
 
 class PostAdapter(): RecyclerView.Adapter<PostAdapter.ViewHolder>() {
 
     /**
      * This is an adapter to Display the post details here we need to pass in the array of the
-     * elements as an argument, to the adapater and render the result
+     * elements as an argument, to the adapter and render the result
      */
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
-
+        val mainLayout: LinearLayout = view.findViewById(R.id.post_ll)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -23,7 +25,10 @@ class PostAdapter(): RecyclerView.Adapter<PostAdapter.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        //  TODO:: Here we will be integrating the UI Elements with the data received
+        holder.mainLayout.setOnClickListener{
+            val intent = Intent(holder.itemView.context, PostDetailsActivity::class.java)
+            holder.itemView.context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
