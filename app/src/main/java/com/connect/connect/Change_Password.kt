d4545d3fdb333a96dc9e99203ada37fb01ed6c1b
CREATORS <harshulwalia217@gmail.com>
2022-05-05 11:10:01 +0530

    Created the UI for change password page

--- app/src/main/java/com/connect/connect/Change_Password.kt
+++ app/src/main/java/com/connect/connect/Change_Password.kt
@@ -2,16 +2,10 @@ package com.connect.connect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.CheckBox
 
-class Register : AppCompatActivity() {
+class Change_Password : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
+        setContentView(R.layout.activity_change_password)
     }
 }
