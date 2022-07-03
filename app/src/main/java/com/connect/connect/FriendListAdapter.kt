c2cd7d7336280814a16c6d6f5de5b6d96ea0a34a
Blake2912 <kcvarun29122001@gmail.com>
2022-07-03 12:33:24 +0530

    Added Initiate Friend Requests Added Remove Friend Request function, but backend implementation is friend

--- app/src/main/java/com/connect/connect/FriendListAdapter.kt
+++ app/src/main/java/com/connect/connect/FriendListAdapter.kt
@@ -1,15 +1,23 @@
 package com.connect.connect
 
+import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.DefaultRetryPolicy
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
 import com.bumptech.glide.Glide
 import de.hdodenhof.circleimageview.CircleImageView
+import org.json.JSONObject
+import java.util.concurrent.TimeUnit
 
 class FriendListAdapter(private val friendList:ArrayList<Friend>, private val apiKey:String) : RecyclerView.Adapter<FriendListAdapter.ViewHolder>() {
     class ViewHolder(view: View):RecyclerView.ViewHolder(view){
@@ -41,9 +49,51 @@ class FriendListAdapter(private val friendList:ArrayList<Friend>, private val ap
             val intent = Intent(holder.itemView.context, UserDetailActivity::class.java)
             holder.itemView.context.startActivity(intent.putExtra("user_id",friend.user_id))
         }
+
+        holder.removeFriendBtn.setOnClickListener{
+//            removeFriend(apiKey,friend.user_id,holder.itemView.context)
+            Toast.makeText(holder.itemView.context,"Under Development!",Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun getItemCount(): Int {
         return friendList.size
+    }
+
+    private fun removeFriend(apiKey:String, friendId:String,context: Context){
+        val url = "https://connect-api-social.herokuapp.com/user/update-friend-request"
+        val stringRequest: StringRequest = object: StringRequest(
+            Method.POST,
+            url,
+            Response.Listener {
+                    response ->
+                val responseJson = JSONObject(response)
+                if(responseJson.getInt("status") == 201){
+//                    val resultObj: JSONObject = responseJson.getJSONObject("result")
+                    Toast.makeText(context,"Friend Successfully Removed!",Toast.LENGTH_LONG).show()
+                }
+                else{
+                    Log.wtf("res",response)
+                    Toast.makeText(context,"Please try again later",Toast.LENGTH_LONG).show()
+                }
+            }, Response.ErrorListener {
+                    error ->
+                Log.d("Volley Error",error.toString())
+            }){
+            override fun getParams(): MutableMap<String, String> {
+                val parameters: MutableMap<String, String> = HashMap()
+                // Add your parameters in HashMap
+                parameters["api_key"] = apiKey
+                parameters["friend_id"] = friendId
+                parameters["request_status"] = "REJECTED"
+                return parameters
+            }
+        }
+        stringRequest.retryPolicy = DefaultRetryPolicy(
+            TimeUnit.SECONDS.toMillis(20).toInt(),  //After the set time elapses the request will timeout
+            1,
+            DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+        )
+        SingletonRequestQueue.getInstance(context).addToRequestQueue(stringRequest)
     }
 }
